775b429445bbbd0ade0e946320568b40292a7639
Vianka Rivera <viankalrivera@gmail.com>
2020-05-12 14:52:53 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,22 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
+
   end
 
   def question?
+    self.end_with?("?")
 
   end
 
   def exclamation?
+    self.end_with?("!")
 
   end
 
   def count_sentences
-
+    sentence_array = self.split(/[.?!]/).reject {|string| string.empty?}
+    return sentence_array.count
   end
 end
